f7292816d9a7436552ab5aa3e1cd22aa6a4c7e5e
Ryan <junheng102@gmail.com>
2018-01-01 18:38:34 +0800

    fix unable to login via facebook at sessions-controller file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,23 @@
 class User < ApplicationRecord
-  include Clearance::User
+ include Clearance::User
 
-  has_many :authentications, dependent: :destroy
 
-    def self.create_with_auth_and_hash(authentication, auth_hash)
-      user = self.create!(
-        email: auth_hash["extra"]["raw_info"]["email"]
-        password: SecureRandom.hex(16)
-      )
-      user.authentications << authentication
-      return user
-    end
+# In app/models/user.rb    
 
-    # grab fb_token to access Facebook for user data
-    def fb_token
-      x = self.authentications.find_by(provider: 'facebook')
-      return x.token unless x.nil?
-    end
+   has_many :authentications, dependent: :destroy
+
+   def self.create_with_auth_and_hash(authentication, auth_hash)
+     user = self.create!(
+       email: auth_hash["extra"]["raw_info"]["email"],
+       password: SecureRandom.hex(16)
+     )
+     user.authentications << authentication
+     return user
+   end
+
+   # grab fb_token to access Facebook for user data
+   def fb_token
+     x = self.authentications.find_by(provider: 'facebook')
+     return x.token unless x.nil?
+   end
 end
