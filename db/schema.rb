5b335017bfb7feb2f493bd60b682ede8216b1396
Ryan <junheng102@gmail.com>
2018-01-01 14:15:52 +0800

    migrate to change password field to be nullable

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101024610) do
+ActiveRecord::Schema.define(version: 20180101060704) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
-    t.string "encrypted_password", limit: 128, null: false
+    t.string "encrypted_password", limit: 128
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.index ["email"], name: "index_users_on_email"
